81120f1a5c46fe7c050660aeada39df8f710bfb6
neAnasteisha <nastyazilvova@gmail.com>
2024-10-19 00:08:53 +0300

    Початок вінка з запланованими платежами

--- Presentation/ExpensesWindow.xaml.cs
+++ Presentation/ExpensesWindow.xaml.cs
@@ -28,5 +28,10 @@ namespace Presentation
         {
             Close();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
