0655ad96fdb7f2170853f881e48d5c920544fa89
Роман Шмигельський <roma.shmyhelskyi@gmail.com>
2024-10-26 01:09:19 +0300

    Додав назви текстбоксам із EntryWindow, Validators, EmailValidationRule

---
+++ Presentation/Validators/EmailValidationRule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Presentation.Validators
+{
+    public class EmailValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            string input = value as string;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return new ValidationResult(false, "Поле не може бути порожнім!");
+            }
+
+            if (!System.Text.RegularExpressions.Regex.IsMatch(input, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                return new ValidationResult(false, "Некоректний формат електронної пошти!");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
