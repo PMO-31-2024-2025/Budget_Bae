69f05fe8510d2c26ca5ebdf81f3f11e7210c713b
Роман Шмигельський <roma.shmyhelskyi@gmail.com>
2024-10-26 23:33:46 +0300

    UserService added

--- BusinessLogic/Services/UserService.cs
+++ BusinessLogic/Services/UserService.cs
@@ -28,14 +28,56 @@ namespace BusinessLogic.Services
             var user = new User
             {
                 Email = email,
-                Name = name,
-                Password = password
+                Password = password,
+                Name = name
             };
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
             return true;
+        }
+
+        public async Task<User> AuthenticateUserAsync(string email, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+            if (user == null)
+            {
+                throw new Exception("Неправильна пошта, або пароль");
+            }
+
+            return user;
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            var existingUser = await _context.Users.FindAsync(user.Id);
+            if (existingUser != null)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                existingUser.Password = user.Password;
+
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new Exception("Користувача не знайдено.");
+            }
+        }
+
+        public async Task DeleteUserAsync(int userId)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if(user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new Exception("Користувача не знайдено.");
+            }
         }
     }
 }
--- Presentation/EntryWindow.xaml.cs
+++ Presentation/EntryWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using DAL.Data;
+﻿using BusinessLogic.Services;
+using DAL.Data;
 using DAL.Models;
 using System;
 using System.Collections.Generic;
@@ -61,72 +62,47 @@ namespace Presentation
 
         private async void settingsRegistrationButton_Click(object sender, RoutedEventArgs e)
         {
-            //    string emailInput = settingsRegistrationEmailTextBox.Text;
-            //    string nameInput = settingsRegistrationNameTextBox.Text;
-            //    string createPasswordInput = settingsRegistrationCreatePasswordTextBox.Text;
-            //    string confirmPasswordInput = settingsRegistrationConfirmPasswordTextBox.Text;
+            string emailInput = settingsRegistrationEmailTextBox.Text;
+            string nameInput = settingsRegistrationNameTextBox.Text;
+            string createPasswordInput = settingsRegistrationCreatePasswordTextBox.Text;
+            string confirmPasswordInput = settingsRegistrationConfirmPasswordTextBox.Text;
 
-            //    if (emailInput == "" || nameInput == "" || createPasswordInput == "" || confirmPasswordInput == "")
-            //    {
-            //        MessageBox.Show("Усі поля мають бути заповнені!");
-            //    }
-            //    else if (!Regex.IsMatch(emailInput, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
-            //    {
-            //        settingsRegistrationEmailTextBox_LostFocus(sender, e);
-            //    }
-            //    else if (createPasswordInput != confirmPasswordInput)
-            //    {
-            //        MessageBox.Show("Паролі мають бути однаковими!");
-            //    }
-            //    else
-            //    {
-            //        try
-            //        {
-            //            var userService = new UserService(new DAL.Data.BudgetBaeContext());
-            //            await userService.RegisterUserAsync(emailInput, nameInput, createPasswordInput);
-            //            MessageBox.Show("Реєстрація успішна!", "Успіх!", MessageBoxButton.OK, MessageBoxImage.Information);
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            MessageBox.Show(ex.Message, "Помилка!", MessageBoxButton.OK, MessageBoxImage.Error);
-            //        }
-            //    }
-            string[] names = {
-            "Марина", "Олександр", "Катерина", "Дмитро", "Анастасiя",
-            "Iван", "Ольга", "Сергiй", "Валентина", "Руслан",
-            "Тетяна", "Светлана", "Вiктор", "Аліна", "Єгор",
-            "Наталiя", "Станіслав", "Ксенія", "Олег", "Свiтлана",
-            "Юлiя", "Михайло", "Роман", "Дарина", "Леонiд",
-            "Артем", "Тимур", "Iрина", "Наталя", "Данило",
-            "Полiна", "Ярослав", "Вадим", "Злата", "Олена"
-        };
+            if (emailInput == "" || nameInput == "" || createPasswordInput == "" || confirmPasswordInput == "")
+            {
+                MessageBox.Show("Усі поля мають бути заповнені!");
+            }
+            else if (!Regex.IsMatch(emailInput, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                settingsRegistrationEmailTextBox_LostFocus(sender, e);
+            }
+            else if (createPasswordInput != confirmPasswordInput)
+            {
+                MessageBox.Show("Паролі мають бути однаковими!");
+            }
+            else
+            {
+                try
+                {
+                    var userService = new UserService(new DAL.Data.BudgetBaeContext());
+                    await userService.RegisterUserAsync(emailInput, nameInput, createPasswordInput);
+                    MessageBox.Show("Реєстрація успішна!", "Успіх!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Помилка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
 
-            // Масив прізвищ
-            string[] surnames = {
-            "Шевченко", "Коваленко", "Таран", "Бондаренко", "Гриценко",
-            "Іваненко", "Сидоренко", "Мельник", "Лисенко", "Кравченко",
-            "Козак", "Кузьменко", "Семенко", "Руденко", "Петренко",
-            "Ковалев", "Тимошенко", "Савченко", "Лебедєв", "Чорненький",
-            "Нечитайло", "Степаненко", "Франко", "Пономаренко", "Зінченко",
-            "Буряк", "Гончар", "Українець", "Федоренко", "Кочерга"
-        };
             try
             {
-                for (int i = 0; i < 37; i++)
-                {
-                    Random random = new Random();
-                    string name = $"{names[random.Next(names.Length)]} {surnames[random.Next(surnames.Length)]}";
-                    string email = $"user{i}@gmail.com";
-                    string password = $"password{i}";
 
-                    User user = new User
-                    {
-                        Name = name,
-                        Email = email,
-                        Password = password
-                    };
-                    DbHelper.db.Users.Add(user);
-                }
+                User user = new User
+                {
+                    Name = name,
+                    Email = email,
+                    Password = password
+                };
+                DbHelper.db.Users.Add(user);
                 DbHelper.db.SaveChanges();
                 MessageBox.Show("Додано успішно!");
             }
