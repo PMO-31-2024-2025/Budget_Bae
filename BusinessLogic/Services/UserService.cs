101366c18fb897de2a3649eb980b8b0170bf058a
Роман Шмигельський <roma.shmyhelskyi@gmail.com>
2024-10-26 03:09:17 +0300

    Я зібрався робити базу з нуля, і все ще за допомогою трохи тупого db first підходу. так просто чуть швидше вийде, бо я маю збережений код для створення. Тому, цей комміт має стати моїм порятунком, якщо щось піде не так. Строго не судіть, нічого кращого не зміг придумати. Якщо робити Code first, то треба ше розбиратися як всю базу робити там

--- BusinessLogic/Services/UserService.cs
+++ BusinessLogic/Services/UserService.cs
@@ -1,14 +1,41 @@
-﻿using System;
+﻿using DAL.Data;
+using DAL.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 
 namespace BusinessLogic.Services
 {
     public class UserService
     {
-        private readonly 
+        private readonly BudgetBaeContext _context;
+        
+        public UserService(BudgetBaeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> RegisterUserAsync(string email, string name, string password)
+        {
+            if (await _context.Users.AnyAsync(u => u.Email == email))
+            {
+                throw new Exception("Користувач з такою електронною поштою вже існує!");
+            }
+
+            var user = new User
+            {
+                Email = email,
+                Name = name,
+                Password = password
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
     }
 }
--- Presentation/EntryWindow.xaml.cs
+++ Presentation/EntryWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using BusinessLogic.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -37,6 +39,58 @@ namespace Presentation
                 var bindingExpression = textBox.GetBindingExpression(TextBox.TextProperty);
                 bindingExpression?.UpdateSource();
                 settingsEntryEmailTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private void settingsRegistrationEmailTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string emailInput = settingsRegistrationEmailTextBox.Text;
+            
+            if (!Regex.IsMatch(emailInput, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                settingsRegistrationEmailTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                MessageBox.Show("Неправильний формат електронної адреси!");
+                
+            }
+            else
+            {
+                settingsRegistrationEmailTextBox.BorderBrush = new SolidColorBrush(Colors.Green);
+            }
+            
+        }
+
+        private async void settingsRegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            string emailInput = settingsRegistrationEmailTextBox.Text;
+            string nameInput = settingsRegistrationNameTextBox.Text;
+            string createPasswordInput = settingsRegistrationCreatePasswordTextBox.Text;
+            string confirmPasswordInput = settingsRegistrationConfirmPasswordTextBox.Text;
+
+            if (emailInput == "" || nameInput == "" || createPasswordInput == "" || confirmPasswordInput == "")
+            {
+                MessageBox.Show("Усі поля мають бути заповнені!");
+            }
+            else if (!Regex.IsMatch(emailInput, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                settingsRegistrationEmailTextBox_LostFocus(sender, e);
+            }
+            else if (createPasswordInput != confirmPasswordInput)
+            {
+                MessageBox.Show("Паролі мають бути однаковими!");
+            }
+            else
+            {
+                try
+                {
+                    var userService = new UserService(new DAL.Data.BudgetBaeContext());
+                    await userService.RegisterUserAsync(emailInput, nameInput, createPasswordInput);
+                    MessageBox.Show("Реєстрація успішна!", "Успіх!", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Помилка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                
             }
         }
     }
