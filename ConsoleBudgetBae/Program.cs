b6435c163ce9c6963a1020b0ef29cd7aac79e08d
neAnasteisha <nastyazilvova@gmail.com>
2024-11-03 12:48:24 +0200

    Так треба

--- ConsoleBudgetBae/Program.cs
+++ ConsoleBudgetBae/Program.cs
@@ -72,13 +72,13 @@ namespace ConsoleLevel
             //});
             //DbHelper.db.SaveChanges();
 
-            DbHelper.db.Add(new PlannedExpense()
-            {
-                Name = "спотіфай",
-                PlannedSum = 120.00,
-                NotigicationDate = "2024-11-01",
-                UserId = 21
-            });
+            //DbHelper.db.Add(new PlannedExpense()
+            //{
+            //    Name = "спотіфай",
+            //    PlannedSum = 120.00,
+            //    NotigicationDate = "2024-11-01",
+            //    UserId = 21
+            //});
 
             //DbHelper.db.SaveChanges();
 
